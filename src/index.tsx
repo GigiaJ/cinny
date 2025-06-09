4af54629f792d7a2b089409bb492e78bd30b597a
Gigiaj <gigiaboone@yahoo.com>
2025-06-08 20:13:59 -0500

    clean up imports

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,9 @@ import { enableMapSet } from 'immer';
 import '@fontsource/inter/variable.css';
 import 'folds/dist/style.css';
 import { configClass, varsClass } from 'folds';
-
-enableMapSet();
-
 import './index.scss';
-
 import { trimTrailingSlash } from './app/utils/common';
 import App from './app/pages/App';
-
-// import i18n (needs to be bundled ;))
 import './app/i18n';
 
 enableMapSet();
