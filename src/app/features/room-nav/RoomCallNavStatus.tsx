a5a9e72c5fedcfb0e526d4c0a4c4c830875ceeb3
Jaggar <18173108+GigiaJ@users.noreply.github.com>
2025-06-15 23:30:47 -0500

    Update src/app/features/room-nav/RoomCallNavStatus.tsx
    Co-authored-by: Gimle Larpes <97182804+GimleLarpes@users.noreply.github.com>

--- src/app/features/room-nav/RoomCallNavStatus.tsx
+++ src/app/features/room-nav/RoomCallNavStatus.tsx
@@ -96,7 +96,7 @@ export function CallNavStatus() {
           )}
         </TooltipProvider>
 
-        <Box grow="Yes">
+        <Box grow="Yes" justifyContent="Center" alignItems="Center">
           <TooltipProvider
             position="Top"
             offset={4}
