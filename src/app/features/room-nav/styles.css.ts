4cbe91a803aa4d39d81beb6e2bdf3900a976d6ae
Gigiaj <gigiaboone@yahoo.com>
2025-06-17 17:47:13 -0500

    Category buttons shouldn't be highlightable

--- src/app/features/room-nav/styles.css.ts
+++ src/app/features/room-nav/styles.css.ts
@@ -3,6 +3,8 @@ import { config } from 'folds';
 
 export const CategoryButton = style({
   flexGrow: 1,
+  userSelect: 'none',
+  WebkitUserSelect: 'none',
 });
 export const CategoryButtonIcon = style({
   opacity: config.opacity.P400,
