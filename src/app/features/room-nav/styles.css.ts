01c3b23284a83b2c563139c29b534923b9d45d98
Gigiaj <gigiaboone@yahoo.com>
2025-06-17 20:26:07 -0500

    revert

--- src/app/features/room-nav/styles.css.ts
+++ src/app/features/room-nav/styles.css.ts
@@ -3,8 +3,6 @@ import { config } from 'folds';
 
 export const CategoryButton = style({
   flexGrow: 1,
-  userSelect: 'none',
-  WebkitUserSelect: 'none',
 });
 export const CategoryButtonIcon = style({
   opacity: config.opacity.P400,
