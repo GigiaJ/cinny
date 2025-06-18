5d8beb1ecad7a2f5d7aac062563d95af5af2f306
Gigiaj <gigiaboone@yahoo.com>
2025-06-18 01:14:13 -0500

    add mobile context menu

--- src/app/pages/client/sidebar/SpaceTabs.tsx
+++ src/app/pages/client/sidebar/SpaceTabs.tsx
@@ -479,6 +479,21 @@ function SpaceTab({
               }
             />
           )}
+          {createPortal(
+            <MobileContextMenu
+              onClose={() => {
+                setMobileSheetOpen(false);
+              }}
+              isOpen={isMobileSheetOpen}
+            >
+              <SpaceMenu
+                room={space}
+                requestClose={() => setMobileSheetOpen(false)}
+                onUnpin={onUnpin}
+              />
+            </MobileContextMenu>,
+            document.body
+          )}
         </SidebarItem>
       )}
     </RoomUnreadProvider>
